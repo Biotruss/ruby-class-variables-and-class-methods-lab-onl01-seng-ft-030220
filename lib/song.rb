e6010dfc833cca56a7a34579526fae5be5b484d5
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-12 20:05:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,28 @@ class Song
   attr_reader :name, :artist, :genre
   
   @@count = 0 
-  @@artists = 0 
-  @@ganres = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
+    @@count += 1
     @artist = artist
+    @@artists += 1
     @genre = genre
+    @@genres += 1
+  end
+  
+  def count
+    @@count
+  end
+  
+  def artists
+    @@artists
+  end
+  
+  def genres
+    @@genres
   end
   
   
